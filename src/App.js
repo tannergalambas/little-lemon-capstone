6fe53cd791985117a0e12a01ce3195218aa24818
tannergalambas <tanner.galambas@yahoo.com>
2025-04-25 22:01:33 -0500

    Add Header and Footer components, clean starter app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer'; // now add Footer
 
 function App() {
   return (
     <div className="App">
-      Homepage
+      <Header />
+      <main>
+        Homepage
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+// src/components/Header.js
+
+function Header() {
+    return (
+      <header>
+        <h1>Little Lemon</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
